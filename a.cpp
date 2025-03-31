2969ced668eff2987c0c59b6afa6702db421deda
Apurbo73 <apurbodebnath895@gmail.com>
2025-03-31 19:59:25 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+
+    cout << "Alphabets from a - z are:" << endl;
+
+    for (char i = 'a'; i <= 'z'; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
